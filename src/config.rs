ea0e9f7af441687e72553881069ba61b90f40cc4
VIISEVEN27 <1035695237@qq.com>
2025-11-11 01:13:06 +0800

    refactor(config): 优化模型配置
    - 移除 OpenAIConfig 结构体的 Deserialize 派生

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,9 @@
-use serde::Deserialize;
+pub enum ModelConfig {
+    OpenAI(OpenAIConfig),
+}
 
-#[derive(Deserialize, Clone)]
 pub struct OpenAIConfig {
     pub model: String,
     pub base_url: String,
     pub api_key: String,
-}
-
-pub enum ModelConfig {
-    OpenAI(OpenAIConfig),
 }
